56661203d72c78c78f357dd0df93e186c2b22e60
Milind Mishra⚡️ <milind.mishra4@gmail.com>
2022-05-23 11:28:10 +0530

    📦 NEW: added get thumbnail on enter

--- script.js
+++ script.js
@@ -1,6 +1,20 @@
 // https://www.youtube.com/watch?v=BfsI6F9_izY
 // https://i.ytimg.com/vi/BfsI6F9_izY/maxresdefault.jpg
 
+// Get the input field
+var input = document.getElementById("video-link");
+
+// Execute a function when the user presses a key on the keyboard
+input.addEventListener("keypress", function (event) {
+  // If the user presses the "Enter" key on the keyboard
+  if (event.key === "Enter") {
+    // Cancel the default action, if needed
+    event.preventDefault();
+    // Trigger the button element with a click
+    document.getElementById("download-btn").click();
+  }
+});
+
 let download = () => {
   // Get the data from element on the form.
   const videoLink = document.getElementById("video-link").value;
