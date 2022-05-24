64131c92c51a709f9428d79e86b4f4692cbbd86a
Milind Mishra⚡️ <milind.mishra4@gmail.com>
2022-05-24 14:42:47 +0530

    🐛 FIX: changes fixed

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
 // https://www.youtube.com/watch?v=BfsI6F9_izY
 // https://i.ytimg.com/vi/BfsI6F9_izY/maxresdefault.jpg
 
-// Get the input field
-var input = document.getElementById("video-link");
+let download = () => {
+  // Get the data from element on the form.
+  const videoLink = document.getElementById("video-link").value;
+  const videoId = videoLink.split("=")[1];
+  console.log(`Video id : ${videoId}`);
+  window.open(`https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`, "_blank");
+};
 
-const url = input.window.location.href;
-console.log(url);
-// getting video id from url : https://milind.live/yth?https://www.youtube.com/watch?v=BfsI6F9_izY
-const videoId = videoLink.slice(videoLink.length - 11);
-window.open(`https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`, "_blank");
+let reset = () => {
+  // Reset the form.
+  document.getElementById("video-link").value = "";
+};
 
 // Execute a function when the user presses a key on the keyboard
 input.addEventListener("keypress", function (event) {
@@ -20,16 +24,3 @@ input.addEventListener("keypress", function (event) {
     document.getElementById("download-btn").click();
   }
 });
-
-let download = () => {
-  // Get the data from element on the form.
-  const videoLink = document.getElementById("video-link").value;
-  const videoId = videoLink.slice(videoLink.length - 11);
-  console.log(`Video id : ${videoId}`);
-  window.open(`https://i.ytimg.com/vi/${videoId}/maxresdefault.jpg`, "_blank");
-};
-
-let reset = () => {
-  // Reset the form.
-  document.getElementById("video-link").value = "";
-};
